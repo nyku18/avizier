e5db197ec868d8aa973ac6102a20361f6de166cd
nyku18 <nicolae.raulea18@gmail.com>
2017-05-14 16:13:14 +0300

    Connect to database

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name = "avizier";
+$connection = mysqli_connect($host, $user, $pass, $db_name);//conexiune la baza de date
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">		
+		<title> Avizier examene </title>
+	</head>
+	
+	<body>
+		<img src=img/facultate.png alt="Cladire facultate inginerie sibiu" />
+		<img src=img/logo.png alt="Logo ULBS" />
+
+
+
+
+	</body>
+
+</html>
